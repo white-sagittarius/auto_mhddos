93f83ee5ac22904f9e9d4d08c549cc8a594a1f65
Aruiem234 <41838573+Aruiem234@users.noreply.github.com>
2022-03-17 10:11:20 +0200

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -44,7 +44,7 @@ rm -rf mhddos_proxy
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd mhddos_proxy
 rm proxies_config.json
-wget https://github.com/Aruiem234/mhddosproxy/blob/main/proxies_config.json
+wget https://raw.githubusercontent.com/Aruiem234/mhddosproxy/main/proxies_config.json
 git clone https://github.com/MHProDev/MHDDoS.git
 python3 -m pip install -r MHDDoS/requirements.txt
 
