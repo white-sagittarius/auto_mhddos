de76253a239f0df45af4f4e6809afa94b3f09f8d
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-03-19 15:35:59 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 
-# Install git, python3, pip if doesn't installed already
-apt update -y
-if [ ! -f /usr/bin/git ]; then
-   sudo apt install git -y
-fi
-if [ ! -f /usr/bin/python3 ]; then
-   sudo apt install python3 -y
-fi
-if [ ! -f /usr/bin/pip ]; then
-   apt install python3-pip  -y
-fi
-if [ ! -f /usr/bin/wget ]; then
-   apt install wget  -y
-fi
-if [ ! -f /usr/bin/curl ]; then
-   apt install curl  -y
-fi
-pip install --upgrade pip > /dev/null #No output. Resolved some problems with pip on debian
+sudo apt update -y
+sudo apt upgrade -y
+# Install git, python3
+sudo apt install git -y
+sudo apt upgrade git -y
+sudo apt install wget -y
+sudo apt upgrade wget -y
+sudo apt install python3 -y
+sudo apt upgrade python3 -y
+sudo apt install python3-pip -y
+sudo apt upgrade python3-pip -y
+
+
 
 #Install latest version of mhddos_proxy and MHDDoS
 cd ~
-rm -rf mhddos_proxy
+sudo rm -r mhddos_proxy
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd mhddos_proxy
-rm proxies_config.json
-wget https://raw.githubusercontent.com/Aruiem234/mhddosproxy/main/proxies_config.json
-git clone https://github.com/MHProDev/MHDDoS.git
-python3 -m pip install -r MHDDoS/requirements.txt
+sudo rm proxies_config.json
+sudo wget https://raw.githubusercontent.com/Aruiem234/mhddosproxy/main/proxies_config.json
+sudo git clone https://github.com/MHProDev/MHDDoS.git
+sudo python3 -m pip install -r MHDDoS/requirements.txt
+cd ~
